3cf386f62cc22ae21be915a0ed00019d8ab9153e
Samuelwps <samuelrip230@gmail.com>
2022-06-28 21:39:09 -0300

    Conecting with API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import styles from "./home.module.scss"
-import  Head  from '../../node_modules/next/head'
+import  Head  from 'next/head'
+import { GetStaticProps } from 'next'
 import { SubscribeButton } from "../components/SubscribeButton/index"
 
+import { stripe } from "../services/services"
 
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    price_id: string;
+    amount: string;
+  }
+}
+
+export default function Home({product}:HomeProps) {
+  console.log(product)
   return (
     <>
     <Head>
@@ -16,13 +27,27 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton/>
       </section> 
       <img src="/images/Mulher.svg" alt="Girl coding" />
     </main>
-
     </>
   )
+}
+
+export const getStaticProps:GetStaticProps = async () =>{
+  const price = await stripe.prices.retrieve("price_1LFQkJKX9J5n4MlXJ9Lb3MN6")
+  
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount / 100),
+  }
+
+  return{
+    props:{
+      product,
+    }
+  }
 }
