61a3dbf608408e62c8d62e0da24f622f7b8902de
Samuelwps <samuelrip230@gmail.com>
2022-07-02 17:51:14 -0300

    Creating connection with Fauna and castrating user in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,42 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { query as q }from "faunadb"
+import {fauna} from "../../../services/faubadb"
+
+
+
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        
       authorization:{
         params:{
           scope: "read:user"
         }
       },
     }),
-    // ...add more providers here
   ],
+  secret: process.env.JWT_KEY,
+  callbacks: {
+    async signIn({ user, account, profile}) {
+
+      const {email} = user
+      
+      try{
+        await fauna.query(
+          q.Create(
+            q.Collection("users"),
+            {data : {email}}
+          )
+        )
+        return true
+      } catch {
+        return true
+      }
+    },
+  }
 })
