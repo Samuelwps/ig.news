1146467c4b3681c2d005a131d94946b6d3b6e841
Samuelwps <samuelrip230@gmail.com>
2022-07-03 18:31:02 -0300

    Redirecting to stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,30 @@
 import styled from "./styled.module.scss"
 import { useSession } from 'next-auth/react';
 import { signIn } from 'next-auth/react';
+import { api } from './../../services/api';
+import { getStripeJs } from "../../services/stripe-js";
 
 
 export function SubscribeButton(){
     const session = useSession()
 
 
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if(!session){
           signIn("github")  
           return;
+        }
+
+        try{
+            const response = await api.post("/subscribe")
+
+            const {sessionId} = response.data
+            
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+        } catch (err) {
+            alert(err.message)
         }
     }
 
