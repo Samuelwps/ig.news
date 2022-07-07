7f166204c3cf367494a173fba286dc6be50ded52
Samuelwps <samuelrip230@gmail.com>
2022-07-07 20:38:02 -0300

    Save subscription in the faunadb

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,44 @@
+import { SlowBuffer } from "buffer";
+import { log } from "console";
+import {query as q, query} from "faunadb"
+import Stripe from "stripe";
+
+import { fauna } from "../../../services/faubadb";
+import { stripe } from "../../../services/services";
+
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string
+){
+    const userRef = await fauna.query(
+        q.Select(
+            "ref",
+            q.Get(
+                q.Match(
+                    q.Index("user_by_stripe_customer_id"),
+                    customerId
+                )
+            )
+        )
+    )
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id
+    }
+
+    console.log(subscriptionData)
+
+    await fauna.query(
+        q.Create(
+            q.Collection("subscription"),
+            { data: subscriptionData}
+        )
+    )
+    
+}
+
